c8c30a7bfe15e3552c1e8895536a6ee9affee3ff
mcsans <mihsansaepulloh9@gmail.com>
2024-04-26 22:02:13 +0700

    finish konsep dasar crud

---
+++ resources/views/mahasiswa/index.blade.php
@@ -0,0 +1,43 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  </head>
+  <body>
+
+    <div class="container mt-5">
+        <button class="btn btn-dark mb-3">Tambah Data</button>
+        <table class="table">
+            <thead>
+              <tr>
+                <th scope="col">#</th>
+                <th scope="col">NIM</th>
+                <th scope="col">Name</th>
+                <th scope="col">Jurusan</th>
+                <th scope="col">Action</th>
+              </tr>
+            </thead>
+            <tbody>
+            @foreach ($mahasiswas as $mahasiswa)
+                <tr>
+                    <th>{{ $loop->iteration }}</th>
+                    <td>{{ $mahasiswa->nim }}</td>
+                    <td>{{ $mahasiswa->name }}</td>
+                    <td>{{ $mahasiswa->jurusan }}</td>
+                    <td>
+                        <button class="btn btn-sm btn-primary">view</button>
+                        <button class="btn btn-sm btn-success">update</button>
+                        <button class="btn btn-sm btn-danger">delete</button>
+                    </td>
+                </tr>
+            @endforeach
+            </tbody>
+          </table>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+  </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,75 @@
 <?php
 
+use App\Http\Controllers\MahasiswaController;
+use App\Models\Mahasiswa;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+// MVC CRUD
+Route::get('/mahasiswa', [MahasiswaController::class, 'index']);
+
+
+// ==========================================================================================
+
+
+// CREATE
+Route::get('/konsep-dasar/create', function () { // http://127.0.0.1:8000/konsep-dasar/create
+    $mahasiswa = Mahasiswa::create([
+        'nim' => '21110336',
+        'name' => 'M Advie Rifaldy',
+        'jurusan' => 'Teknik Informatika',
+    ]);
+
+    return $mahasiswa;
+});
+
+// READ
+Route::prefix('/konsep-dasar/read')->group(function () {
+    Route::get('/all', function () { // http://127.0.0.1:8000/konsep-dasar/read/all
+        // get all
+        $mahasiswas = Mahasiswa::all();
+
+        return $mahasiswas;
+    });
+
+    Route::get('/show', function () { http://127.0.0.1:8000/konsep-dasar/read/show
+        // get by id
+        $mahasiswa = Mahasiswa::find(3);
+        // $mahasiswa = Mahasiswa::where('id', 3)->first(); sama dengan (Mahasiswa::find(3);)
+
+        return $mahasiswa;
+    });
+
+    Route::get('/search/{keyword}', function ($keyword) { //http://127.0.0.1:8000/konsep-dasar/read/search/advie
+        // get search
+        $mahasiswa = Mahasiswa::where('nim', 'LIKE', '%' . $keyword . '%')
+                                ->orWhere('name', 'LIKE', '%' . $keyword . '%')
+                                ->orWhere('jurusan', 'LIKE', '%' . $keyword . '%')
+                                ->get();
+
+        return $mahasiswa;
+    });
+});
+
+// UPDATE
+Route::get('/konsep-dasar/update', function () { // http://127.0.0.1:8000/konsep-dasar/update
+    $id = 2;
+
+    $data = [
+        'nim' => '21110333',
+        'name' => 'Faisal Dzulfikar',
+        'jurusan' => 'Teknik Informatika',
+    ];
+
+    Mahasiswa::where('id', $id)->update($data);
+
+    return Mahasiswa::find(2);
+});
+
+// DELETE
+Route::get('/konsep-dasar/delete', function () { // http://127.0.0.1:8000/konsep-dasar/update
+    $id = 1;
+
+    Mahasiswa::where('id', $id)->delete();
+
+    return Mahasiswa::all();
 });
